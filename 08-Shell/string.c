aec0c6de6136217beab2fbd6000a368e04513317
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-08-31 00:28:40 +0800

    Implement strtok
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- 08-Shell/string.c
+++ 08-Shell/string.c
@@ -30,3 +30,33 @@ size_t strlen(const char* s)
         while (*s++) ++result;
         return result;
 }
+
+char* strtok(char *str, const char* delim)
+{
+        static char* bp;
+
+        if(str != NULL) bp = str;
+
+        if(bp[0] == '\0') return NULL;
+
+        char *ret = bp, *b;
+        const char *d;
+
+        for(b = bp; *b !='\0'; b++) {
+                for(d = delim; *d != '\0'; d++) {
+                        if(*b == *d) {
+                                *b = '\0';
+                                bp = b+1;
+
+                                // skip the beginning delimiters
+                                if(b == ret) {
+                                        ret++;
+                                        continue;
+                                }
+                                return ret;
+                        }
+                }
+        }
+
+        return ret;
+}
--- 08-Shell/string.h
+++ 08-Shell/string.h
@@ -4,5 +4,6 @@
 int strcmp(char *s1, char *s2);
 int strncmp(char *s1, char *s2, int n);
 size_t strlen(const char* s);
+char *strtok(char *str, const char* delim);
 
 #endif /* __STRING_H__ */
